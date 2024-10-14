adada8282dce2ed978f747ea0691a4cac0485318
VICTOIRE90 <20ms359@esisalama.org>
2024-10-14 17:56:31 +0200

    Projet-1

---
+++ index.php
@@ -0,0 +1,132 @@
+<?php include 'src/header.php'; ?>
+   
+
+    <!-- services -->
+    <div class="spacer services wowload fadeInUp">
+        <div class="container">
+            <div class="row">
+            <!-- notre carte -->
+                <div class="col-sm-4">
+                <!-- RoomCarousel -->
+                <div id="TourCarousel" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="item active"><img src="images/photos/6.jpg" class="img-responsive" alt="slide"></div>
+                        <div class="item  height-full"><img src="images/photos/3.jpg"  class="img-responsive" alt="slide"></div>
+                        <div class="item  height-full"><img src="images/photos/4.jpg"  class="img-responsive" alt="slide"></div>
+                    </div>
+                    <!-- Controls -->
+                    <a class="left carousel-control" href="#TourCarousel" role="button" data-slide="prev"><i class="fa fa-angle-left"></i></a>
+                    <a class="right carousel-control" href="#TourCarousel" role="button" data-slide="next"><i class="fa fa-angle-right"></i></a>
+                </div>
+                <!-- RoomCarousel-->
+                <div class="py-4 px-4 shadow rounded bg-white">
+                    <div class="d-flex justify-content-between mb-3">
+                        <h5 class="mb-0">Super Deluxe</h5>
+                        <div class="ps-2">
+                            <small class="fa fa-star text-primary"></small>
+                            <small class="fa fa-star text-primary"></small>
+                            <small class="fa fa-star text-primary"></small>
+                            <small class="fa fa-star text-primary"></small>
+                            <small class="fa fa-star text-primary"></small>
+                        </div>
+                    </div>
+                    <div class="d-flex mb-3">
+                        <small class="border-end me-3 pe-3"><i class="fa fa-bed text-primary me-2"></i>3 Bed</small>
+                        <small class="border-end me-3 pe-3"><i class="fa fa-bath text-primary me-2"></i>2 Bath</small>
+                        <small class="border-end me-3 pe-3"><i class="fa fa-wifi text-primary me-2"></i>Wifi</small>
+                        <small class="bg-primary text-white rounded">$100/Night</small>
+                    </div>
+                        <p class="text-body mb-3">Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet lorem.</p>
+                    <div class="d-flex justify-content-between">
+                        <a class="btn btn-sm btn-primary rounded py-2 px-4" href="">View Detail</a>
+                        <a class="btn btn-sm btn-dark rounded py-2 px-4" href="">Book Now</a>
+                    </div>
+                </div>
+            </div>
+            <!-- notre carte fin -->
+
+
+            <!-- notre carte -->
+            <div class="col-sm-4">
+                <!-- RoomCarousel -->
+            <div id="TourCarousel1" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="item active"><img src="images/photos/6.jpg" class="img-responsive" alt="slide"></div>
+                    <div class="item  height-full"><img src="images/photos/3.jpg"  class="img-responsive" alt="slide"></div>
+                    <div class="item  height-full"><img src="images/photos/4.jpg"  class="img-responsive" alt="slide"></div>
+                </div>
+                    <!-- Controls -->
+                    <a class="left carousel-control" href="#TourCarousel1" role="button" data-slide="prev"><i class="fa fa-angle-left"></i></a>
+                    <a class="right carousel-control" href="#TourCarousel1" role="button" data-slide="next"><i class="fa fa-angle-right"></i></a>
+            </div>
+                <!-- RoomCarousel-->
+                <div class="py-4 px-4 shadow rounded bg-white">
+                    <div class="d-flex justify-content-between mb-3">
+                        <h5 class="mb-0">Super Deluxe</h5>
+                        <div class="ps-2">
+                            <small class="fa fa-star text-primary"></small>
+                            <small class="fa fa-star text-primary"></small>
+                            <small class="fa fa-star text-primary"></small>
+                            <small class="fa fa-star text-primary"></small>
+                            <small class="fa fa-star text-primary"></small>
+                        </div>
+                    </div>
+                    <div class="d-flex mb-3">
+                        <small class="border-end me-3 pe-3"><i class="fa fa-bed text-primary me-2"></i>3 Bed</small>
+                        <small class="border-end me-3 pe-3"><i class="fa fa-bath text-primary me-2"></i>2 Bath</small>
+                        <small class="border-end me-3 pe-3"><i class="fa fa-wifi text-primary me-2"></i>Wifi</small>
+                        <small class="bg-primary text-white rounded">$100/Night</small>
+                    </div>
+                        <p class="text-body mb-3">Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet lorem.</p>
+                    <div class="d-flex justify-content-between">
+                        <a class="btn btn-sm btn-primary rounded py-2 px-4" href="">View Detail</a>
+                        <a class="btn btn-sm btn-dark rounded py-2 px-4" href="">Book Now</a>
+                    </div>
+                </div>
+            </div>
+            <!-- notre carte fin -->
+
+
+            <!-- notre carte -->
+            <div class="col-sm-4">
+                <!-- RoomCarousel -->
+            <div id="TourCarousel2" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="item active"><img src="images/photos/6.jpg" class="img-responsive" alt="slide"></div>
+                    <div class="item  height-full"><img src="images/photos/3.jpg"  class="img-responsive" alt="slide"></div>
+                    <div class="item  height-full"><img src="images/photos/4.jpg"  class="img-responsive" alt="slide"></div>
+                </div>
+                    <!-- Controls -->
+                    <a class="left carousel-control" href="#TourCarousel2" role="button" data-slide="prev"><i class="fa fa-angle-left"></i></a>
+                    <a class="right carousel-control" href="#TourCarousel2" role="button" data-slide="next"><i class="fa fa-angle-right"></i></a>
+            </div>
+                <!-- RoomCarousel-->
+                <div class="py-4 px-4 shadow rounded bg-white">
+                    <div class="d-flex justify-content-between mb-3">
+                        <h5 class="mb-0">Super Deluxe</h5>
+                        <div class="ps-2">
+                            <small class="fa fa-star text-primary"></small>
+                            <small class="fa fa-star text-primary"></small>
+                            <small class="fa fa-star text-primary"></small>
+                            <small class="fa fa-star text-primary"></small>
+                            <small class="fa fa-star text-primary"></small>
+                        </div>
+                    </div>
+                    <div class="d-flex mb-3">
+                        <small class="border-end me-3 pe-3"><i class="fa fa-bed text-primary me-2"></i>3 Bed</small>
+                        <small class="border-end me-3 pe-3"><i class="fa fa-bath text-primary me-2"></i>2 Bath</small>
+                        <small class="border-end me-3 pe-3"><i class="fa fa-wifi text-primary me-2"></i>Wifi</small>
+                        <small class="bg-primary text-white rounded">$100/Night</small>
+                    </div>
+                        <p class="text-body mb-3">Erat ipsum justo amet duo et elitr dolor, est duo duo eos lorem sed diam stet diam sed stet lorem.</p>
+                    <div class="d-flex justify-content-between">
+                        <a class="btn btn-sm btn-primary rounded py-2 px-4" href="">View Detail</a>
+                        <a class="btn btn-sm btn-dark rounded py-2 px-4" href="">Book Now</a>
+                    </div>
+                </div>
+            </div>
+            <!-- notre carte fin -->
+        </div>
+    </div>
+    
+<?php include 'src/footer.php'; ?>
---
+++ src/header.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Hotelier - Hotel HTML Template</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href=" " rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600;700&family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">  
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+    <!-- Google fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Raleway:300,500,800|Old+Standard+TT' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:300,500,800' rel='stylesheet' type='text/css'>
+
+    <!-- font awesome -->
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+
+    <!-- uniform -->
+    <link type="text/css" rel="stylesheet" href="assets/uniform/css/uniform.default.min.css" />
+
+    <!-- animate.css -->
+    <link rel="stylesheet" href="assets/wow/animate.css" />
+
+
+    <!-- gallery -->
+    <link rel="stylesheet" href="assets/gallery/blueimp-gallery.min.css">
+
+
+    <!-- favicon -->
+    <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+    <link rel="icon" href="images/favicon.png" type="image/x-icon">
+
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+
+<body>
